01b14861c987cca43116b9b85efd23418e0ae818
João Vidal <dev.joaovidal@gmail.com>
2022-09-10 19:14:13 -0300

    better treatment to promotion

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -41,7 +42,13 @@ public class Program {
 
                 if (chessMatch.getPromoted() != null) {
                     System.out.print("Enter piece for promotion ( B / N / R / Q ): ");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+                        System.out.print("Enter piece for promotion ( B / N / R / Q ): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
+
                     chessMatch.replacePromotedPiece(type);
                 }
             }
